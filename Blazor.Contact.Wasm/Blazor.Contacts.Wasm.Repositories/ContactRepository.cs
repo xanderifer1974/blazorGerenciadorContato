1216795dec84c3b081f6e02c12844a0195b0d001
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-28 14:39:02 -0300

    Ajustes para a model

--- Blazor.Contact.Wasm/Blazor.Contacts.Wasm.Repositories/ContactRepository.cs
+++ Blazor.Contact.Wasm/Blazor.Contacts.Wasm.Repositories/ContactRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Blazor.Contacts.Wasm.Repositories
+﻿using System.Data;
+
+namespace Blazor.Contacts.Wasm.Repositories
 {
     public class ContactRepository: IContactRepository
     {
+        private readonly IDbConnection _dbConnection;
+
+        public ContactRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
     }
 }
--- Blazor.Contact.Wasm/Blazor.Contacts.Wasm.Repositories/IContactRepository.cs
+++ Blazor.Contact.Wasm/Blazor.Contacts.Wasm.Repositories/IContactRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Blazor.Contacts.Wasm.Repositories
+﻿using Blazor.Contacts.Wasm.Repositories.Models;
+using System.Threading.Tasks;
+
+namespace Blazor.Contacts.Wasm.Repositories
 {
     public  interface IContactRepository
     {
+        Task<bool> InsertContact(Contact contato);
     }
 }
