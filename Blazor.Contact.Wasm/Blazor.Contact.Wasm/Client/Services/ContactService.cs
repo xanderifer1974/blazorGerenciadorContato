f8565b5f9cae054a21d3f82e355ce0ddd71d67ef
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-11-29 16:38:50 -0300

    Criando A interface de serviço e iniciando o serviço

--- Blazor.Contact.Wasm/Blazor.Contact.Wasm/Client/Services/ContactService.cs
+++ Blazor.Contact.Wasm/Blazor.Contact.Wasm/Client/Services/ContactService.cs
@@ -1,6 +1,38 @@
-﻿namespace Blazor.Contact.Wasm.Client.Services
+﻿using Blazor.Contact.Wasm.Shared;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blazor.Contact.Wasm.Client.Services
 {
-    public class ContactService: IContactService
+    public class ContactService : IContactService
     {
+        private readonly HttpClient _httpClient;
+
+        public ContactService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public Task DeleteContact(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contato>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Contato> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SaveContact(Contato contact)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Blazor.Contact.Wasm/Blazor.Contact.Wasm/Client/Services/IContactService.cs
+++ Blazor.Contact.Wasm/Blazor.Contact.Wasm/Client/Services/IContactService.cs
@@ -1,6 +1,14 @@
-﻿namespace Blazor.Contact.Wasm.Client.Services
+﻿using Blazor.Contact.Wasm.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Blazor.Contact.Wasm.Client.Services
 {
     public interface IContactService
     {
+        Task SaveContact(Contato contact);
+        Task DeleteContact(int id);
+        Task<IEnumerable<Contato>> GetAll();
+        Task<Contato> GetById(int id);
     }
 }
